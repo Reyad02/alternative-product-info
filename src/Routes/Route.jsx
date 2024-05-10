c82ca1286c81190a0b5c25c4b7edf23cdd605e5e
unknown <areyad203011@bscse.uiu.ac.bd>
2024-05-10 20:41:21 +0600

    navbar and footer component created

--- src/Routes/Route.jsx
+++ src/Routes/Route.jsx
@@ -1,8 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
+import Root from "../Layout/Root";
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <div>Hello world!</div>,
+        element: <Root></Root>,
     },
 ]);
 
