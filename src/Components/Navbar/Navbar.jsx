f3f80449353461243edd392ade1d5c0e7876d20d
unknown <areyad203011@bscse.uiu.ac.bd>
2024-05-10 23:12:41 +0600

    login part and navbar done

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,15 +1,34 @@
-import { NavLink } from 'react-router-dom';
+import { useContext } from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import { AuthContext } from '../../Providers/AuthProvider';
 
 const Navbar = () => {
+    const { user, logout } = useContext(AuthContext);
     const navlinks = <>
         <li><NavLink to="/">Home</NavLink></li>
         <li><NavLink to="/quires">Queries</NavLink></li>
-        <li><NavLink to="/login">Login</NavLink></li>
+        {user && <>
+            <li><NavLink to="/recommendations">Recommendations</NavLink></li>
+            <li><NavLink to="/myQuires">My Quires</NavLink></li>
+            <li><NavLink to="/myRecommendations">My Recommendations</NavLink></li>
+
+        </>}
     </>
+
+    const handleLogout = () => {
+        logout().then(() => {
+            // Sign-out successful.
+            console.log("Successfully logged out")
+        }).catch((error) => {
+            // An error happened.
+            console.log("error");
+        });
+
+    }
     return (
         <div className='max-w-7xl mx-auto'>
             <div className="navbar bg-base-100">
-                <div className="navbar-start w-1/3">
+                <div className="navbar-start ">
                     <div className="dropdown">
                         <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
@@ -21,20 +40,16 @@ const Navbar = () => {
                     </div>
                     <a className="btn btn-ghost text-xl">daisyUI</a>
                 </div>
-                <div className="navbar-center hidden w-1/3 lg:flex justify-center">
+                <div className="navbar-center hidden lg:flex justify-center">
                     <ul className="menu menu-horizontal px-1">
                         {navlinks}
                     </ul>
                 </div>
-                <div className="dropdown dropdown-end ml-auto">
-                    <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-                        <div className="w-10 rounded-full">
-                            <img alt="Tailwind CSS Navbar component" src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
-                        </div>
-                    </div>
-                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                        <li><a>Logout</a></li>
-                    </ul>
+                <div className="navbar-end">
+                    {
+                        (user && <a className="btn" onClick={handleLogout}>Logout</a>) || <Link to="/login">Login</Link>
+                    }
+
                 </div>
             </div>
         </div>
--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,21 +1,41 @@
-import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../Providers/AuthProvider";
 
 const Login = () => {
+    const { login } = useContext(AuthContext);
+    const location = useLocation();
+    const navigate = useNavigate();
+    const handleLogin = (e) => {
+        e.preventDefault();
+        const form = e.target;
+        const email = form.email.value;
+        const pass = form.pass.value;
+        login(email, pass).then((userCredential) => {
+            const user = userCredential.user;
+            navigate(location?.state ? location?.state : "/")
+        })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                console.log(errorMessage)
+            });
+    }
     return (
-        <div className="max-w-7xl mx-auto flex justify-center">
-            <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                <form className="card-body">
+        <div className="max-w-7xl mx-auto flex justify-center mb-8">
+            <div className="card shrink-0 w-full max-w-sm py-10 shadow-2xl bg-base-100">
+                <form className="card-body" onSubmit={handleLogin}>
                     <div className="form-control">
                         <label className="label">
                             <span className="label-text">Email</span>
                         </label>
-                        <input type="email" placeholder="email" className="input input-bordered" required />
+                        <input type="email" name="email" placeholder="email" className="input input-bordered" required />
                     </div>
                     <div className="form-control">
                         <label className="label">
                             <span className="label-text">Password</span>
                         </label>
-                        <input type="password" placeholder="password" className="input input-bordered" required />
+                        <input type="password" name="pass" placeholder="password" className="input input-bordered" required />
                         {/* <label className="label">
                             <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                         </label> */}
@@ -24,10 +44,8 @@ const Login = () => {
                         <button className="btn btn-primary">Login</button>
                     </div>
                 </form>
-                <p>If you don't have an account then <Link to="/signup">Register</Link> first.</p>
-                
+                <p className="text-center">If you don't have an account then <Link to="/signup" className="text-blue-500">Register</Link> first.</p>
             </div>
-
         </div>
     );
 };
